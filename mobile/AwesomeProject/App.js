e9d3dbaa2aa7beea1fbf68519ce7d36f2a39fc0c
wildangbudhi <wildangb@gmail.com>
2019-12-15 16:06:19 +0700

    Update Mobile App

--- mobile/AwesomeProject/App.js
+++ mobile/AwesomeProject/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import {StyleSheet, SafeAreaView, FlatList, View, Text, Button, Animated} from 'react-native';
-import Swipeable from 'react-native-gesture-handler/Swipeable';
+import GestureRecognizer, {swipeDirections} from 'react-native-swipe-gestures';
 import io from 'socket.io-client'
 
 const style = StyleSheet.create({
     container : {
         flex: 1,
-        backgroundColor: '#fff',
+        backgroundColor: '#000',
         alignItems: "center",
         justifyContent: "center"
     },
@@ -19,30 +19,72 @@ const style = StyleSheet.create({
 
 export default class App extends React.Component
 {
-    constructor()
-    {
-        super();
-        this.socket = io('http://192.168.2.2:3000');
-    }
 
-    componentDidMount()
-    {
-        this.socket.emit('direction', 'right');
-    }
+    constructor(props) {
+        super(props);
+        this.socket = io('https://habib-tampan.azurewebsites.net');
+        this.state = {
+            myText: 'I\'m ready to get swiped!',
+            gestureName: 'none',
+            backgroundColor: '#fff'
+          };
+        
+      }
 
-    sendDirection(dir)
-    {
+      sendDir(dir)
+      {
         this.socket.emit('direction', dir);
-    }
+      }
+      
+      componentDidMount()
+      {
+        this.socket.emit('direction', 'right');
+      }
+    
+      onSwipeUp(gestureState) {
+        this.socket.emit('direction', 'up');
+        this.setState({myText: 'You swiped up!'});
+      }
+    
+      onSwipeDown(gestureState) {
+        this.socket.emit('direction', 'down');
+        this.setState({myText: 'You swiped down!'});
+      }
+    
+      onSwipeLeft(gestureState) {
+        this.socket.emit('direction', 'left');
+        this.setState({myText: 'You swiped left!'});
+      }
+    
+      onSwipeRight(gestureState) {
+        this.socket.emit('direction', 'right');
+        this.setState({myText: 'You swiped right!'});
+      }
+    
+    
+      render() {
+    
+        const config = {
+          velocityThreshold: 0.05,
+          directionalOffsetThreshold: 100
+        };
+    
+        return (
+          <GestureRecognizer
+            onSwipeUp={ this.onSwipeUp.bind(this) }
+            onSwipeDown={ this.onSwipeDown.bind(this) }
+            onSwipeLeft={ this.onSwipeLeft.bind(this) }
+            onSwipeRight={ this.onSwipeRight.bind(this) }
+            config={config}
+            style={{
+              flex: 1,
+              backgroundColor: this.state.backgroundColor
+            }}
+            >
+                <Text style={ {alignContent:"center"}}>{this.state.myText}</Text>
+          </GestureRecognizer>
 
-    render()
-    {
-        return(
-            <View style={style.container}>
-               <Swipeable>
 
-               </Swipeable>
-            </View>
         );
-    }
+      }
 }
